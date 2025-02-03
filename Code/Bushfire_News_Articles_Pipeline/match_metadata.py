ee2d7266932e4220824591553340fa70c3e75d36
Finnoscarmorgan <54052996+Finnoscarmorgan@users.noreply.github.com>
2025-02-03 16:53:22 +1100

    Update match_metadata.py

--- Code/Bushfire_News_Articles_Pipeline/match_metadata.py
+++ Code/Bushfire_News_Articles_Pipeline/match_metadata.py
@@ -3,11 +3,11 @@
 import pandas as pd
 import time
 
-entities_df = pd.read_csv('/Users/fiannualamorgan/Documents/GitHub/Historical_Fires_Near_Me/Sections/Bushfire_Literature/Cleaned_Data/Bushfire_Literature_Input_Disambiguator_Output.csv')
-metadata_df = pd.read_csv('/Users/fiannualamorgan/Documents/GitHub/Historical_Fires_Near_Me/Sections/Bushfire_Literature/Cleaned_Data/Bushfire_Location_Spreadsheet_July_19.csv')
+entities_df = pd.read_csv('PATH_NAME')
+metadata_df = pd.read_csv('PATH_NAME')
 
 merged_df = entities_df.set_index('article_id').join(metadata_df.set_index('article_id'))
 #merged_df.rename({'Unnamed: 0': 'article_id'}, axis=1, inplace=True)
 
-merged_df.to_csv('/Users/fiannualamorgan/Documents/GitHub/Historical_Fires_Near_Me/Sections/Bushfire_Literature/Cleaned_Data/Bushfire_Literature_Metadata_Disambiguated.csv', index=False)
+merged_df.to_csv('PATH_NAME', index=False)
 
